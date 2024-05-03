bc1083d6e55d01f3a967d3c4aa91da0dba5265f4
Kartik <kartikagarwal20144@gmail.com>
2024-05-03 22:05:36 +0530

    New changes

--- src/Components/Posts.jsx
+++ src/Components/Posts.jsx
@@ -8,9 +8,13 @@ const Posts = () => {
   return (
     <div className="posts-center position-relative">
         <Post1 />
+        <br />
         <Post2 />
+        <br />
         <Post3 />
+        <br />
         <Post4 />
+        <br />
           </div>
   )
 }
