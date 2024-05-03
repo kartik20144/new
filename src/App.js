0b956ec1257a4ed0c3a886b7485f00d66b530bfa
Kartik <kartikagarwal20144@gmail.com>
2024-05-03 12:28:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HomeScreen from './screens/HomeScreen';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+  return (
+   
+ <HomeScreen />
+  );
+}
+
+export default App;
